6ad4f24630bb457f2188cf172ecc0fe4f1470c59
”Brandtned” <mara0008@stud.kea.dk>
2024-02-20 15:38:19 +0100

    Ændret margin i footer

--- js/svamp.js
+++ js/svamp.js
@@ -1,9 +1,8 @@
-//Project URL: 
-//https://wkvrpgxhbgrztjimigfp.supabase.co
+// henter oh identificerer ID for det produkt der klikkes på og definerer to konstanter
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
 
-//API nøgle:
-//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndrdnJwZ3hoYmdyenRqaW1pZ2ZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgyNzQwMjUsImV4cCI6MjAyMzg1MDAyNX0.geECteCf-DjNUNftn-g6ltFF63AugdIYWMg3Jc-pgHM
-
+//henter data for det korrekte id
 fetch("https://wkvrpgxhbgrztjimigfp.supabase.co/rest/v1/Svampedata",{
     method:"GET",
     headers:{
@@ -11,53 +10,17 @@ fetch("https://wkvrpgxhbgrztjimigfp.supabase.co/rest/v1/Svampedata",{
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndrdnJwZ3hoYmdyenRqaW1pZ2ZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgyNzQwMjUsImV4cCI6MjAyMzg1MDAyNX0.geECteCf-DjNUNftn-g6ltFF63AugdIYWMg3Jc-pgHM"
     },
 })
+
 .then((res) => res.json())
 .then(showProducts);
 
 
-
-
-const urlParams = new URLSearchParams(window.location.search);
-const id = urlParams.get ("id");
-
-function getProdcut() {
-fetch("https://kea-alt-del.dk/t7/api/products/"+id)
-    .then((response) => response.json())
-    .then ((data) => showProduct(data));
+function showProduct(items){
+    console.log(items);
 }
 
+//gør så dataen ikke kommer som et array
+const singleProduct = items[0];
 
-function showProduct(product){
-    let productClone;
-    
-    console.log(product);
-    document.querySelector(".product_name").textContent = product.productdisplayname;
-    document.querySelector(".sold_out_box").textContent = product.soldout;
-    document.querySelector(".product_image2").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
-    document.querySelector(".product_image2").alt = `Picture of a ${product.productdisplayname} product`;
-    document.querySelector(".product_tagline").textContent = product.tagline;
-    document.querySelector(".badge").textContent = product.brandname;
-    document.querySelector(".price").textContent = `Prev. ${product.price} DKK ,-`;
-    document.querySelector(".offer2").textContent = `-${product.discount} % `;
-
-    // Ændre indhold i breadcrumbs
-  document.querySelector(".breadcrumbs li:nth-child(3) a").textContent = product.category;
-  document.querySelector(".breadcrumbs li:nth-child(3) a").href = `productlist.html?category=${product.category}`
-  document.querySelector(".breadcrumbs li:nth-child(4)").textContent = product.brandname;
-  document.querySelector(".breadcrumbs li:nth-child(5)").textContent = product.productdisplayname;
-
-    if(product.soldout) {
-        document.querySelector(".sold_out_box").textContent = 'Udsolgt';
-      }
-      else {document.querySelector(".sold_out_box").classList.add("hide")}
-      
-      document.querySelector(".product_image2").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
-      document.querySelector(".product_image2").alt = `Picture of a ${product.productdisplayname} product`;
-      
-      if (product.discount){
-        document.querySelector(".offer2").textContent = `-${product.discount} % `;
-      }
-      else {document.querySelector(".offer2").classList.add("hide")}
-}
-
-getProdcut();
+//Udfyld indhold med den rigtige data
+document.querySelector(".overskrift_svamp").textContent=singleProduct.Svampenavn;
